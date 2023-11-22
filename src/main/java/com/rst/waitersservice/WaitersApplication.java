33a6ad96fc044263d95381bb05394f6c860ad8d3
OleksandrFliagin <samme0062@gamil.com>
2023-11-22 21:22:46 +0200

    init

---
+++ src/main/java/com/rst/waitersservice/WaitersApplication.java
@@ -0,0 +1,13 @@
+package com.rst.waitersservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WaitersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WaitersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rst/waitersservice/WaitersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rst.waitersservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WaitersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
